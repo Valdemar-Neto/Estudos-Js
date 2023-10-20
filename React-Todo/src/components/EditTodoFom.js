ba67aa4a3c8a67570febf02660c4054e0ce4f1db
Valdemar Neto <valldemar@live.com>
2023-10-19 22:10:58 -0300

    Estudos de React

---
+++ React-Todo/src/components/EditTodoFom.js
@@ -0,0 +1,20 @@
+import React, {useState}from 'react'
+
+export const EditTodoForm = ({editTodo, task}) => {
+    const [value, setvalue] = useState(task.task)
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        editTodo(value, task.id );
+
+        setvalue("")
+    }
+    return(
+        <form className = "TodoForm" onSubmit = {handleSubmit}>
+            <input type = "text" className="todo-input" value = {value}
+            placeholder="Atualize a tarefa" onChange={(e) => setvalue(e.target.value)}></input>
+            <button type="submit" className="todo-btn">Update</button>
+        </form>
+    )
+}
---
+++ React-Todo/src/components/TodoForm.js
@@ -0,0 +1,20 @@
+import React, {useState}from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setvalue] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        addTodo(value);
+
+        setvalue("")
+    }
+    return(
+        <form className = "TodoForm" onSubmit = {handleSubmit}>
+            <input type = "text" className="todo-input" value = {value}
+            placeholder="Qual a tarefa da vez?" onChange={(e) => setvalue(e.target.value)}></input>
+            <button type="submit" className="todo-btn">Add Task</button>
+        </form>
+    )
+}
